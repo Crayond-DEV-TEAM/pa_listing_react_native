6bb755316064727a539abad9579043470e057eb0
“hariram” <hariram@crayond.co>
2023-08-30 19:00:24 +0530

    ios build given to testflight

--- ios/com.app.pa-listingsgotoTests/com.app.pa-listingsgotoTests.m
+++ ios/com.app.pa-listingsgotoTests/com.app.pa-listingsgotoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface app.pa_listingsgotoTests : XCTestCase
+@interface com.app.pa-listingsgotoTests : XCTestCase
 
 @end
 
-@implementation app.pa_listingsgotoTests
+@implementation com.app.pa-listingsgotoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
