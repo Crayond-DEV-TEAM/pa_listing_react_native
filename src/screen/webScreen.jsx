c6b4db787b95107d09d6eb9d8165dea0de6a4ec5
“hariram” <hariram@crayond.co>
2023-10-30 16:05:52 +0530

    listing app fix the location info.plist file

--- src/screen/webScreen.jsx
+++ src/screen/webScreen.jsx
@@ -28,9 +28,65 @@ const INJECTED_JAVASCRIPT = `(function() {
     window.ReactNativeWebView.postMessage(getItemLocalStorage);
 })();`;
 
+export const getGeoLocationJS = () => {
+    const getCurrentPosition = `
+      navigator.geolocation.getCurrentPosition = (success, error, options) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'getCurrentPosition', options: options }));
+  
+        window.addEventListener('message', (e) => {
+          let eventData = {}
+          try {
+            eventData = JSON.parse(e.data);
+          } catch (e) {}
+  
+          if (eventData.event === 'currentPosition') {
+            success(eventData.data);
+          } else if (eventData.event === 'currentPositionError') {
+            error(eventData.data);
+          }
+        });
+      };
+      true;
+    `;
+
+    const watchPosition = `
+      navigator.geolocation.watchPosition = (success, error, options) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'watchPosition', options: options }));
+  
+        window.addEventListener('message', (e) => {
+          let eventData = {}
+          try {
+            eventData = JSON.parse(e.data);
+          } catch (e) {}
+  
+          if (eventData.event === 'watchPosition') {
+            success(eventData.data);
+          } else if (eventData.event === 'watchPositionError') {
+            error(eventData.data);
+          }
+        });
+      };
+      true;
+    `;
+
+    const clearWatch = `
+      navigator.geolocation.clearWatch = (watchID) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'clearWatch', watchID: watchID }));
+      };
+      true;
+    `;
+
+    return `
+      (function() {
+        ${getCurrentPosition}
+        ${watchPosition}
+        ${clearWatch}
+      })();
+    `;
+};
 const isIOS = Platform.OS === 'ios';
 const { height } = Dimensions.get('window');
-
+let webview = null;
 const WebScreen = (props) => {
     const [status, setStatus] = React.useState(null);
 
@@ -110,45 +166,76 @@ const WebScreen = (props) => {
     }, [status]);
 
     const WebviewRender = () => {
-
         return <WebView
-            injectedJavaScript={INJECTED_JAVASCRIPT}
-            onMessage={onMessage}
+            injectedJavaScript={getGeoLocationJS()}
+            onMessage={event => {
+                let data = {}
+                try {
+                    data = JSON.parse(event.nativeEvent.data);
+                } catch (e) {
+                    console.log(e);
+                }
+
+                if (data?.event && data.event === 'getCurrentPosition') {
+                    Geolocation.getCurrentPosition((position) => {
+                        webview.postMessage(JSON.stringify({ event: 'currentPosition', data: position }));
+                    }, (error) => {
+                        webview.postMessage(JSON.stringify({ event: 'currentPositionError', data: error }));
+                    }, data.options);
+                } else if (data?.event && data.event === 'watchPosition') {
+                    Geolocation.watchPosition((position) => {
+                        webview.postMessage(JSON.stringify({ event: 'watchPosition', data: position }));
+                    }, (error) => {
+                        webview.postMessage(JSON.stringify({ event: 'watchPositionError', data: error }));
+                    }, data.options);
+                } else if (data?.event && data.event === 'clearWatch') {
+                    Geolocation.clearWatch(data.watchID);
+                }
+            }}
+            ref={ref => {
+                webview = ref;
+                // if (onRef) {
+                //   onRef(webview)
+                // }
+            }}
             overScrollMode='never'
             pullToRefreshEnabled={true}
-            onLoadEnd={() => {
-                setVisible(false)
-            }}
-            incognito={true}
-            cacheEnabled={false}
-            cacheMode={'LOAD_NO_CACHE'}
-            source={{ uri: `https://listingsgoto.com`}} style={{ marginTop: isIOS ? 0 : 10 }}
-            renderLoading={() => {
-                if (visible) {
-                    <ActivityIndicator
-                        style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            jusityContent: "space-around",
-                            flexWrap: "wrap",
-                            alignContent: "center",
-                        }}
-                        size="large"
-                    />
+
+            cacheEnabled={true}
+            // cacheMode={'LOAD_NO_CACHE'}
+            source={{ uri: `https://listingsgoto.com` }} style={{ marginTop: isIOS ? 0 : 10 }}
+
+            geolocationEnabled={true}
+            allowsInlineMediaPlayback={true}
+            mediaPlaybackRequiresUserAction={false}
+
+            cacheMode={'LOAD_DEFAULT'}
+            sharedCookiesEnabled={true}
+            setBuiltInZoomControls={false}
+
+            useWebKit
+            originWhitelist={['*']}
+            incognito={false}
+
+            mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
+
+            geolocationPermissionRequest={(request) => {
+                if (true) {
+                    // You have geolocation permission
+                    request.grant();
+                } else {
+                    // You don't have geolocation permission
+                    request.deny();
                 }
-            }
-            }
+            }}
         />
     }
     // 
     return (
-        <View style={{flex: 1,backgroundColor:"#091b29"}}>
-            <SafeAreaProvider style={{flex: 1}}>
-                <StatusBar translucent backgroundColor={"#091b29"} barStyle="light-content"/>
-                <SafeAreaView style={{flex:1, paddingBottom: isIOS && height < 812 ? -1 : -40}}>
+        <View style={{ flex: 1, backgroundColor: "#091b29" }}>
+            <SafeAreaProvider style={{ flex: 1 }}>
+                <StatusBar translucent backgroundColor={"#091b29"} barStyle="light-content" />
+                <SafeAreaView style={{ flex: 1, paddingBottom: isIOS && height < 812 ? -1 : -40 }}>
                     <WebviewRender />
                 </SafeAreaView>
             </SafeAreaProvider>
